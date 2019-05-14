e6c8ee58cfc258b1bcc0af59b6c586ca40aa0573
root <root@node.localdomain>
2019-05-15 08:54:21 +0900

    hoge

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-print "Hello-2, World... from " . $_SERVER['SERVER_ADDR'] . " to " . $_SERVER['REMOTE_ADDR'] . ".\n";
+print "Hello-3, World... from " . $_SERVER['SERVER_ADDR'] . " to " . $_SERVER['REMOTE_ADDR'] . ".\n";
 /*
 print "PHP-2 version: " . PHP_VERSION . "\n";
 print "IP address: " . $_SERVER['SERVER_ADDR'] . "\n";
